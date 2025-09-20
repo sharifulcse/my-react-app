f8f68edf594460e06f6a0e1e36c30eaeefcb4e5c
sharifulcse <shariful.cse.diu@gmail.com>
2025-09-20 11:01:48 +0600

    OK

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
+import Header from "./Header";
 export default function About() {
   return (
     <div>
+      <Header />
       <h1>About</h1>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import Header from "./Header";
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Home Page</h1>
     </div>
   );
